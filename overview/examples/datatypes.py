991e638630bcf6b75342b82d124f5ae27e305600
Pablo Solar Vilariño <psolarvi@redhat.com>
2022-03-17 10:08:51 +0100

    chapter(overview): Added chapter examples and exercise solutions

---
+++ overview/examples/datatypes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+x = 10
+tab = " \t"
+
+print(x, tab, type(x), tab, id(x))
+y = x
+print(y, tab, type(y), tab, id(y))
+x = 25.7
+print(x, tab, type(x), tab, id(x))
+x = "Hello"
+print(x, tab, type(x), tab, id(x))
---
+++ overview/solutions/first.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A solution for Exercise 03 and Exercise 04
+    Exercise 04 requires 'chmod u+x first.py' to enable the
+    script to be executable.
+"""
+
+x = 10
+y = 20
+x = x + 3
+y = y * x
+print(x)
+print(y)
+print(x / y)
