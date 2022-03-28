f4d2d252cdd379fce8ceec64d37e267084dde567
randythomas <randy.thomas@redhat.com>
2022-03-28 12:36:34 -0500

    making the solution match the instructions (#17)
    Co-authored-by: Randy Thomas <althomas@redhat.com>

--- classes/solutions/classes_ex01.py
+++ classes/solutions/classes_ex01.py
@@ -20,6 +20,30 @@ class Person:
         self.age = age
         self.gender = gender
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @gender.setter
+    def gender(self, gender):
+        self._gender = gender
+
     def __str__(self):
         return "{} {} {}".format(self.name, self.age, self.gender)
 
