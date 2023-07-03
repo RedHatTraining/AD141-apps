7e55a9453e28f8e76032ee9d794e56137df19ea5
John Baumlin <git@jab.contact>
2023-07-04 04:38:55 +0900

    First test exercise.

---
+++ basics/practice1-1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+exercise_test = input("Enter some text and we'll calculate the total amount of characters: " )
+
+print("The original text entered was: " + exercise_test + "\n" + "And this totals to: " \
+        + str(len(exercise_test)) + " Characters")
+
