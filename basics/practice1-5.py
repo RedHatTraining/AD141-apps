1c3e073080b2f0ee7d6a2f43f31c6df553bb4bfd
John Baumlin <git@jab.contact>
2023-07-08 05:38:26 +0900

    Calculating with variable types and exponents.

---
+++ basics/practice1-5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+radius = float(input("This script will calculate the area for of a circle for you.\nPlease provide the radius: "))
+
+pi = 3.14159
+area = pi*(pow(radius,2))
+
+print("The area of your circle is: " + str(area))
+
