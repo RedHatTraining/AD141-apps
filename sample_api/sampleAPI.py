70efb3cdc7fd4d944e9aa6888bc5d2dfd5a816ac
Jaime Y <jyague@redhat.com>
2023-07-24 11:55:18 +0200

    more style changes

--- sample_api/sampleAPI.py
+++ sample_api/sampleAPI.py
@@ -11,26 +11,28 @@ class ToDoList(Resource):
     def get(self):
         data = pd.read_csv('todolist.csv')
         data = data.to_dict(orient='records')
-        return {'data':data}, 200
+        return {'data': data}, 200
 
 class Numbers(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('number', type=int, required=True, location="args")
+        parser.add_argument('number', type=int, 
+                            required=True, location='args')
         args = parser.parse_args()
         
         data = pd.read_csv('numbers.csv')
         number = args['number']
-        data2 = data.query("number==@number")
-        data2 = data2.reset_index(drop=True)
-        if data2.shape[0] > 0 :
-            idx = randint(0, data2.shape[0]-1)
-            return {'data':data2['fact'][idx]}, 200
+        number_data = data.query('number==@number')
+        number_data = number_data.reset_index(drop=True)
+        if number_data.shape[0] > 0 :
+            idx = randint(0, number_data.shape[0]-1)
+            return {'data': number_data['fact'][idx]}, 200
         else:
-            return {'data':"No facts for that number. Please select a number between 1 and 10."}, 200
+            return {'data': 'No facts for that number. \
+                    Please select a number between 1 and 10.'}, 200
 
 api.add_resource(ToDoList, '/todolist')
 api.add_resource(Numbers, '/numbers')
 
-if __name__=='__main__':
-    app.run(host = '0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
