7b1fd8bb0355d0cf3284004a650254e583b54d84
Jaime Y <jyague@redhat.com>
2023-07-24 12:18:37 +0200

    and more style improvements

--- sample_api/sampleAPI.py
+++ sample_api/sampleAPI.py
@@ -4,14 +4,17 @@ from flask_restful import Resource, Api, reqparse
 import pandas as pd
 from random2 import randint
 
+
 app = Flask(__name__)
 api = Api(app)
+
 
 class ToDoList(Resource):
     def get(self):
         data = pd.read_csv('todolist.csv')
         data = data.to_dict(orient='records')
         return {'data': data}, 200
+
 
 class Numbers(Resource):
     def get(self):
@@ -22,14 +25,16 @@ class Numbers(Resource):
         
         data = pd.read_csv('numbers.csv')
         number = args['number']
-        number_data = data.query('number==@number')
+        number_data = data.query("number==@number")
         number_data = number_data.reset_index(drop=True)
         if number_data.shape[0] > 0 :
-            idx = randint(0, number_data.shape[0]-1)
+            idx = randint(0, number_data.shape[0] - 1)
+
             return {'data': number_data['fact'][idx]}, 200
-        else:
-            return {'data': 'No facts for that number. \
-                    Please select a number between 1 and 10.'}, 200
+        
+        return {'data': 'No facts for that number. \
+                Please select a number between 1 and 10.'}, 200
+
 
 api.add_resource(ToDoList, '/todolist')
 api.add_resource(Numbers, '/numbers')
